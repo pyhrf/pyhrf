bc310c0ca819788c056f29409cd258c3f47d4b6c
Thomas Vincent <thomas.tv.vincent@gmail.com>
2014-03-11 00:43:32 +0100

    WIP on script for frontiers (fix estimation results according to paper)

--- script/frontiers2014/real_data_jde_rfir_glm.py
+++ script/frontiers2014/real_data_jde_rfir_glm.py
@@ -34,11 +34,14 @@ def main():
 
     contrasts = pyhrf.paradigm.default_contrasts_loc
 
-    condition_of_interest = 'calculaudio'
-    contrast_of_interest = 'computation-sentences'
-    point_of_interest = {'axial':33, 'coronal':25, 'sagittal':53}
-    parcel_of_interest = 11
-    plot_label =  'parietal'
+    experiments = [ ('audio', 'audio-video',
+                     {'axial':17, 'coronal':42, 'sagittal':9}, 1,
+                     'temporal', -1., 1.05),
+                     ('calculaudio', 'computation-sentences',
+                     {'axial':30, 'coronal':25, 'sagittal':53}, 11,
+                     'parietal', -1.55, 1.05),
+                   ]
+
 
     parcellation_dir = get_dir(output_dir, 'parcellation')
 
@@ -47,14 +50,14 @@ def main():
                                     bold_files=[bold_file],
                                     paradigm_csv_file=paradigm_file)
 
+    glm_hcano_output_dir = get_dir(output_dir, 'glm_cano')
     if 0:
         # GLM with canonical HRF
         print 'GLM with canonical HRF'
-        glm_hcano_output_dir = get_dir(output_dir, 'glm_cano')
         glm_analyse(fdata, contrasts, output_dir=glm_hcano_output_dir,
                     output_prefix='glm_hcano_')
 
-    if 1:
+    if 0:
         # GLM with basis set
         print 'GLM with basis set'
         glm_hderiv_output_dir = get_dir(output_dir, 'glm_hderivs')
@@ -62,87 +65,101 @@ def main():
                     output_dir=glm_hderiv_output_dir,
                     output_prefix='glm_hderivs_')
 
-    if 0:
-        # GLM FIR
-        print 'GLM FIR'
-        glm_fir_output_dir = get_dir(output_dir, 'glm_fir')
-        glm_analyse(fdata, contrasts={}, hrf_model="FIR",
-                    output_dir=glm_fir_output_dir, output_prefix='glm_fir_',
-                    fir_delays=range(11))
-
-    # Regularized FIR
-    print 'RFIR'
-    rfir_mask = op.join(parcellation_dir, 'mask_single_voxel_for_rfir.nii')
-    make_single_voxel_mask(point_of_interest, mask_file, rfir_mask)
-    fdata_rfir = FmriData.from_vol_files(mask_file=rfir_mask,
-                                         bold_files=[bold_file],
-                                         paradigm_csv_file=paradigm_file)
-    rfir_output_dir = get_dir(output_dir, 'rfir')
-    rfir_analyse(fdata_rfir, output_dir=rfir_output_dir)
-
-    # parcellation from results of GLM basis set
-
     parcellation_file =  op.join(parcellation_dir,  'parcellation_func.nii')
-    mask_file = op.join(glm_hderiv_output_dir, 'mask.nii')
-    beta_files = [op.join(glm_hderiv_output_dir, '*%s*.nii'%c) \
-                          for c in fdata.get_condition_names()]
-    make_parcellation_from_files(beta_files, mask_file, parcellation_file,
-                                 nparcels=20, method='ward_and_gkm')
+    if 0:
+        # parcellation from results of GLM basis set
 
-    # JDE
-    print 'JDE'
+        mask_file = op.join(glm_hderiv_output_dir, 'glm_hderivs_mask.nii')
+        beta_files = [op.join(glm_hderiv_output_dir,'glm_hderivs_beta_%s.nii'%c)\
+                              for c in fdata.get_condition_names()]
+        make_parcellation_from_files(beta_files, mask_file, parcellation_file,
+                                     nparcels=20, method='ward_and_gkm')
     jde_output_dir = get_dir(output_dir, 'jde')
-    fdata_parc = FmriData.from_vol_files(mask_file=parcellation_file,
-                                         bold_files=[bold_file],
-                                         paradigm_csv_file=paradigm_file)
-    jde_analyse(fdata_parc, jde_output_dir)
+    if 0:
+        # JDE
+        print 'JDE'
+        fdata_parc = FmriData.from_vol_files(mask_file=parcellation_file,
+                                             bold_files=[bold_file],
+                                             paradigm_csv_file=paradigm_file)
+        jde_analyse(fdata_parc, contrasts, jde_output_dir)
 
-    # GLM hcano rescaled onto JDE (provide the same results as normal GLM hcano
-    # but with effects resized so that there is a consistency btw
-    # X^m.h in JDE and the corresponding column of the design matrix in GLM
-    print 'GLM rescaled'
-    rescale_factor_file = op.join(jde_output_dir, 'scale_factor_for_glm.nii')
-    glm_hcano_rs_output_dir = get_dir(output_dir, 'glm_hcano_rescaled_on_jde')
-    glm_analyse(fdata, contrasts, output_dir=glm_hcano_rs_output_dir,
-                output_prefix='glm_hcano_rs_',
-                rescale_factor_file=rescale_factor_file)
+    glm_hcano_rs_output_dir = get_dir(output_dir,'glm_hcano_rescaled_on_jde')
+    if 0:
+        # GLM hcano rescaled onto JDE (provide the same results as normal
+        # GLM hcano but with effects resized so that there is a consistency btw
+        # X^m.h in JDE and the corresponding column of the design matrix in GLM
+        print 'GLM rescaled'
+        rescale_factor_file = op.join(jde_output_dir, 'scale_factor_for_glm.nii')
+        compute_jde_glm_rescaling(jde_output_dir, glm_hcano_output_dir,
+                                  rescale_factor_file)
+
+        glm_analyse(fdata, contrasts, output_dir=glm_hcano_rs_output_dir,
+                    output_prefix='glm_hcano_rs_',
+                    rescale_factor_file=rescale_factor_file)
 
     ## Outputs
+    for condition_of_interest, contrast_of_interest, point_of_interest, \
+        parcel_of_interest, plot_label, ymin, ymax  in experiments:
 
-    fig_dir = get_dir(output_dir, 'figs')
-    plot_detection_results(fig_dir, point_of_interest, condition_of_interest,
-                           contrast_of_interest, jde_output_dir,
-                           glm_hcano_rs_output_dir)
+        if 'temporal' in plot_label:
+            paradigm_tag = 'loc_av'
+        else:
+            paradigm_tag = 'loc'
 
-    plot_estimation_results(fig_dir, point_of_interest, parcel_of_interest,
-                            condition_of_interest, plot_label,
-                            glm_fir_output_dir, rfir_output_dir, jde_output_dir)
+        paradigm_file = get_data_file_name('paradigm_%s.csv' %paradigm_tag)
+        fir_mask = op.join(parcellation_dir, 'mask_single_voxel_for_fir_%s.nii'\
+                           %paradigm_tag)
+        make_mask_from_points([point_of_interest], mask_file, fir_mask)
+        fdata_fir = FmriData.from_vol_files(mask_file=fir_mask,
+                                            bold_files=[bold_file],
+                                            paradigm_csv_file=paradigm_file)
+
+        glm_fir_output_dir = get_dir(output_dir, 'glm_fir_%s' %paradigm_tag)
+        if 1:
+            # GLM FIR
+            print 'GLM FIR'
+            glm_analyse(fdata_fir, contrasts={}, hrf_model="FIR",
+                        output_dir=glm_fir_output_dir, output_prefix='glm_fir_',
+                        fir_delays=range(11))
+
+
+
+        rfir_output_dir = get_dir(output_dir, 'rfir_%s' %paradigm_tag)
+        if 1:
+            # Regularized FIR
+            print 'RFIR'
+            rfir_analyse(fdata_fir, output_dir=rfir_output_dir)
+
+
+        fig_dir = get_dir(output_dir, 'figs')
+        plot_detection_results(fig_dir, point_of_interest, condition_of_interest,
+                               contrast_of_interest, parcellation_file,
+                               plot_label, jde_output_dir,
+                               glm_hcano_rs_output_dir)
+
+        plot_estimation_results(fig_dir, point_of_interest, parcel_of_interest,
+                                condition_of_interest, plot_label,
+                                glm_fir_output_dir, rfir_output_dir,
+                                jde_output_dir, ymin, ymax)
 
 def glm_analyse(fdata, contrasts, output_dir, output_prefix,
-                hrf_model="Canonical", fir_delays=None):
+                hrf_model="Canonical", fir_delays=None,
+                rescale_factor_file=None):
     glm_analyser = GLMAnalyser(hrf_model=hrf_model, contrasts=contrasts,
-                               outputPrefix=output_prefix,
-                               fir_delays=fir_delays)
+                               outputPrefix=output_prefix, fir_delays=fir_delays,
+                               rescale_factor_file=rescale_factor_file)
     glm_analyser.set_pass_errors(False)
     tt = FMRITreatment(fdata, glm_analyser, output_dir=output_dir)
     tt.run()
 
 def jde_analyse(fdata, contrasts, output_dir):
     from pyhrf.jde.models import BOLDGibbsSampler as BG
-    from pyhrf.jde.hrf import RHSampler as HVS
-    from pyhrf.jde.nrl.bigaussian import NRLSampler as NS
+    from pyhrf.jde.hrf import RHSampler
+    from pyhrf.jde.nrl.bigaussian import NRLSampler
 
-    sampler = BG({
-        BG.P_NB_ITERATIONS : 2, # HACK
-        # HRF variance
-        BG.P_RH : HVS({
-            HVS.P_SAMPLE_FLAG : False,
-            HVS.P_VAL_INI : np.array([0.05]),
-            }),
-        BG.P_NRLS : NS({
-            NS.P_CONTRASTS : contrasts,
-            })
-        })
+    sampler = BG(nb_iterations=2, # HACK
+                 hrf_var=RHSampler(do_sampling=False, val_ini=np.array([0.05])),
+                 response_levels=NRLSampler(contrasts=contrasts))
 
     analyser = JDEMCMCAnalyser(sampler=sampler)
     tt = FMRITreatment(fdata, analyser, output_dir=output_dir)
@@ -153,21 +170,131 @@ def rfir_analyse(fdata, output_dir):
     tt = FMRITreatment(fdata, analyser, output_dir=output_dir)
     tt.run(parallel='local')
 
-def make_single_voxel_mask(poi, mask_file, new_mask_file):
+
+def compute_jde_glm_rescaling(jde_path, glm_path, output_file):
+
+    #TODO: check consistency of condition order btwn GLM & JDE !!!
+
+    # load matX from JDE results (same for all parcels)
+    # matX is a matrix of shape (time x nb_hrf_coeff)
+    # matX = sum_m(X^m * m) where X is the matrix defined in the fwd JDE model
+    # jde_dm_fn = op.join(jde_path,'jde_mcmc_matX.nii')
+    # jde_dm = xndarray.load(jde_dm_fn).sub_cuboid(ROI=12).reorient(('time','P'))
+
+    # print 'jde_dm:'
+    # print jde_dm.descrip()
+
+    # ny,lgCI = jde_dm.data.shape
+    # nbConditions = len(np.unique(jde_dm.data)) - 1
+
+    # # reconstruct all X^m from matX
+    # varX = np.zeros((nbConditions,ny,lgCI))
+    # for j in xrange(nbConditions):
+    #     varX[j,:,:] = (jde_dm.data == j).astype(int)
+
+
+    jde_varX_fn = op.join(jde_path,'jde_mcmc_varX.nii')
+    jde_varX = xndarray.load(jde_varX_fn).sub_cuboid(ROI=1)
+    jde_varX = jde_varX.reorient(('condition', 'time','P'))
+
+    print 'jde_dm varX:'
+    print jde_varX.descrip()
+
+    nbConditions,ny,lgCI = jde_varX.data.shape
+    varX = jde_varX.data
+
+    # More convenient matrix structure to perfom product with HRF afterwards
+    stackX = np.zeros((ny*nbConditions,lgCI), dtype=int)
+
+    for j in xrange(nbConditions):
+        stackX[ny*j:ny*(j+1), :] = varX[j,:,:]
+
+    print 'stackX:', stackX.shape
+
+    # Load HRFs from JDE results
+    jde_hrf_fn =  op.join(jde_path, 'jde_mcmc_hrf_pm.nii')
+    jde_hrf = xndarray.load(jde_hrf_fn).reorient(('ROI','time'))
+
+    print 'jde_hrf:'
+    print jde_hrf.descrip()
+
+    roi_ids = jde_hrf.axes_domains['ROI']
+    jde_xh = np.zeros((len(roi_ids),ny,nbConditions))
+
+    for iroi,roi in enumerate(roi_ids):
+        h = jde_hrf.sub_cuboid(ROI=roi).data[1:-1]
+        # make sure that the HRF is normalized:
+        h /= (h**2).sum()**.5
+        stackXh = np.dot(stackX, h)
+        jde_xh[iroi,:,:] = np.reshape(stackXh, (nbConditions,ny)).transpose()
+
+    jde_roi_mask_fn =  op.join(jde_path, 'jde_mcmc_roi_mapping.nii')
+    print 'jde_roi_mask_fn:', jde_roi_mask_fn
+    jde_roi_mask = xndarray.load(jde_roi_mask_fn)
+
+    glm_dm_fn =  op.join(glm_path, 'glm_hcano_design_matrix.nii')
+    glm_dm = xndarray.load(glm_dm_fn).sub_cuboid(ROI=1).reorient(('time',
+                                                                  'regressor'))
+
+    print 'glm_dm:'
+    print glm_dm.descrip()
+
+    # align condition axis of GLM design matrix onto condition axis of JDE
+    # design matrix:
+    jde_xh_tmp = jde_xh.copy()
+    cond_domain = []
+    for cidx,cn in enumerate(glm_dm.axes_domains['regressor']):
+        if cn in jde_varX.axes_domains['condition']:
+            jde_cidx = np.where(jde_varX.axes_domains['condition']==cn)[0][0]
+            print 'cidx:', cidx
+            print 'jde_cidx:', jde_cidx
+            jde_xh_tmp[:,:,cidx] = jde_xh[:,:,jde_cidx]
+            cond_domain.append(cn)
+
+    #glm_norm_weights = (glm_dm.data**2).sum(0)**.5
+    glm_norm_weights = glm_dm.data.ptp(0)
+
+    jde_norm_weights = np.zeros(jde_roi_mask.data.shape + (nbConditions,))
+    scale_factor = np.zeros_like(jde_norm_weights)
+    for iroi,roi in enumerate(roi_ids):
+        m = np.where(jde_roi_mask.data == roi)
+        #jde_w = (jde_xh[iroi]**2).sum(0)**.5
+        jde_w = jde_xh[iroi].ptp(0)
+        jde_norm_weights[m[0],m[1],m[2],:] = jde_w
+        scale_factor[m[0],m[1],m[2],:] =  glm_norm_weights[:len(jde_w)] / jde_w
+
+    from pyhrf.ndarray import MRI3Daxes
+    csf = xndarray(scale_factor, axes_names=MRI3Daxes+['condition'],
+                 axes_domains={'condition':np.array(cond_domain)},
+                 meta_data=jde_hrf.meta_data)
+
+    print 'rescale factor:'
+    print csf.descrip()
+
+    print 'scale_factor file:', output_file
+    csf.reorient(['condition']+MRI3Daxes).save(output_file)
+
+
+def make_mask_from_points(pois, mask_file, new_mask_file):
 
     m = xndarray.load(mask_file)
-    i,j,k = poi['sagittal'], poi['coronal'], poi['axial']
     new_m = xndarray.xndarray_like(m)
-    new_m.data[i,j,k] = 1
+    for poi in pois:
+        i,j,k = poi['sagittal'], poi['coronal'], poi['axial']
+        new_m.data[i,j,k] = 1
+
     new_m.save(new_mask_file)
 
 def plot_detection_results(fig_dir, poi, condition, coi, parcellation_file,
-                           jde_output_dir, glm_hcano_rs_output_dir,
+                           plot_label, jde_output_dir, glm_hcano_rs_output_dir,
                            fig_dpi=100):
     """
     coi (str): contrast of interest
     poi (dict): defines the point of interest for plots of HRFs and maps
     """
+    if condition == 'audio':
+        condition = 'phraseaudio'
+        
     orientation = ['coronal',  'sagittal']
     axial_slice =  poi['axial']
 
@@ -248,8 +375,9 @@ def plot_detection_results(fig_dir, poi, condition, coi, parcellation_file,
 
 def plot_estimation_results(fig_dir, poi, jde_roi, cond, plot_label,
                             glm_fir_output_dir, rfir_output_dir,
-                            jde_output_dir, plot_fontsize=25,
-                            ymin=-1.55, ymax=1.05):
+                            jde_output_dir, ymin=-1.55, ymax=1.05,
+                            plot_fontsize=25):
+
 
     ## HRF plots
 
