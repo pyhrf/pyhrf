Constraint zero in both asl and bold multisession, scaling not inside the processing of a parcel, some other fixes

python/pyhrf/ui/vb_jde_analyser_asl_fast.py
@@ -154,12 +154,7 @@ class JDEVEMAnalyser(JDEAnalyser):
         # roiData.graph #list of neighbours
         n_scan_allsession, nvox = roiData.bold.shape
         n_scan = n_scan_allsession / self.n_session
-        data0 = roiData.bold.reshape(self.n_session, n_scan, nvox)
-        data = np.zeros_like(data0)
-        for s in xrange(self.n_session):
-            data_mean = np.mean(data0[s, :, :])
-            data_range = (np.max(data0[s, :, :]) - np.min(data0[s, :, :]))    
-            data[s, :, :] = (data0[s, :, :] - data_mean) * 100 / data_range    
+        data = roiData.bold.reshape(self.n_session, n_scan, nvox)
         Onsets = roiData.paradigm.get_joined_onsets_dim()
         durations = roiData.paradigm.get_joined_durations_dim()
         TR = roiData.tr
@@ -402,7 +397,9 @@ class JDEVEMAnalyser(JDEAnalyser):
                                         axes_domains=ad,
                                         value_label='Conv_Criterion_C')
             outName = 'convergence_FE'
-            outputs[outName] = xndarray(FE, axes_names=axes_names,
+            c = np.zeros(self.nItMax)  # -.001 #
+            c[:len(FE)] = FE
+            outputs[outName] = xndarray(c, axes_names=axes_names,
                                         value_label='Conv_Criterion_FE')            
             
             logger.info("Convergence saved ")

python/pyhrf/ui/vb_jde_analyser_bold_fast.py
@@ -154,12 +154,7 @@ class JDEVEMAnalyser(JDEAnalyser):
         # roiData.graph #list of neighbours
         n_scan_allsession, nvox = roiData.bold.shape
         n_scan = n_scan_allsession / self.n_session
-        data0 = roiData.bold.reshape(self.n_session, n_scan, nvox)
-        data = np.zeros_like(data0)
-        for s in xrange(self.n_session):
-            data_mean = np.mean(data0[s, :, :])
-            data_range = (np.max(data0[s, :, :]) - np.min(data0[s, :, :]))    
-            data[s, :, :] = (data0[s, :, :] - data_mean) * 100 / data_range    
+        data = roiData.bold.reshape(self.n_session, n_scan, nvox)
         Onsets = roiData.paradigm.get_joined_onsets_dim()
         durations = roiData.paradigm.get_joined_durations_dim()
         TR = roiData.tr
@@ -404,16 +399,7 @@ class JDEVEMAnalyser(JDEAnalyser):
             outputs[outName] = xndarray(c, axes_names=axes_names,
                                         axes_domains=ad,
                                         value_label='Conv_Criterion_FE')            
-            """outName = 'convergence_EP'
-            outputs[outName] = xndarray(EP, axes_names=axes_names,
-                                        value_label='Conv_Criterion_EP')            
-            outName = 'convergence_EPlh'
-            outputs[outName] = xndarray(EPlh, axes_names=axes_names,
-                                        value_label='Conv_Criterion_EPlh')            
-            outName = 'convergence_Ent'
-            outputs[outName] = xndarray(Ent, axes_names=axes_names,
-                                        value_label='Conv_Criterion_Ent')            
-            """
+            
             logger.info("Convergence saved ")
 
         #######################################################################

python/pyhrf/vbjde/vem_asl_models_fast_ms.py
@@ -605,13 +605,17 @@ def Main_vbjde_physio(graph, Y, Onsets, durations, Thrf, K, TR, beta, dt,
         H /= Hnorm
         Sigma_H /= Hnorm**2
         m_A *= Hnorm
+        Sigma_A *= Hnorm**2
+    if not constraint or not normg:
         Gnorm = np.linalg.norm(G)
         G /= Gnorm
         Sigma_G /= Gnorm**2
         m_C *= Gnorm
+        Sigma_C *= Hnorm**2
     
     if zc:
         H = np.concatenate(([0], H, [0]))
+        G = np.concatenate(([0], G, [0]))
 
     ## Compute contrast maps and variance
     if computeContrast and len(contrasts) > 0:
@@ -627,7 +631,7 @@ def Main_vbjde_physio(graph, Y, Onsets, durations, Thrf, K, TR, beta, dt,
     ###########################################################################
     ##########################################    PLOTS and SNR computation
 
-    if PLOT:
+    if PLOT and 0:
         logger.info("plotting...")
         print 'FE = ', FE
         vt.plot_convergence(ni, M, cA, cC, cH, cG, cAH, cCG, SUM_q_Z, mua1, muc1, FE)
@@ -649,9 +653,9 @@ def Main_vbjde_physio(graph, Y, Onsets, durations, Thrf, K, TR, beta, dt,
                 np.linalg.norm(Y[s, :, :] - StimulusInducedSignal - PL[s, :, :])))
     logger.info("SNR = %d",  SNR10)
 
-    return ni, m_A.mean(0), H, m_C.mean(0), G, Z_tilde, sigma_eps[s, :], \
-           mu_Ma, sigma_Ma, mu_Mc, sigma_Mc, Beta, AL[:, :, s], PL[s, :, :], \
-           np.zeros_like(AL[0, :, s]), Sigma_A[:, :, :, s], Sigma_C[:, :, :, s], Sigma_H, Sigma_G, rerror, \
+    return ni, m_A.mean(0), H, m_C.mean(0), G, Z_tilde, sigma_eps.mean(0), \
+           mu_Ma, sigma_Ma, mu_Mc, sigma_Mc, Beta, AL.mean(2), PL.mean(0), \
+           np.zeros_like(AL[0, :, s]), Sigma_A.mean(3), Sigma_C.mean(3), Sigma_H, Sigma_G, rerror, \
            CONTRAST_A, CONTRASTVAR_A, CONTRAST_C, CONTRASTVAR_C, \
            cA[:], cH[2:], cC[2:], cG[2:], cZ[2:], cAH[2:], cCG[2:], \
            cTime, FE

python/pyhrf/vbjde/vem_tools.py
@@ -2456,14 +2456,14 @@ def plot_response_functions_it(ni, NitMin, M, H, G, Mu=None, prior=None):
 
 def plot_convergence(ni, M, cA, cC, cH, cG, cAH, cCG,
                      SUM_q_Z, mua1, muc1, FE):
-    SUM_p_Q_array = np.zeros((M, ni), dtype=np.float64)
+    """SUM_p_Q_array = np.zeros((M, ni), dtype=np.float64)
     mua1_array = np.zeros((M, ni), dtype=np.float64)
     muc1_array = np.zeros((M, ni), dtype=np.float64)
     for m in xrange(M):
         for i in xrange(ni):
             SUM_p_Q_array[m, i] = SUM_q_Z[m][i]
             mua1_array[m, i] = mua1[m][i]
-            muc1_array[m, i] = muc1[m][i]
+            muc1_array[m, i] = muc1[m][i]"""
     
     import matplotlib
     import matplotlib.pyplot as plt
@@ -2489,12 +2489,12 @@ def plot_convergence(ni, M, cA, cC, cH, cG, cAH, cCG,
     plt.legend(('CA', 'CC', 'CH', 'CG'))
     plt.grid(True)
     plt.savefig('./plots/Crit_all.png')
-    plt.figure(M + 6)
+    """plt.figure(M + 6)
     for m in xrange(M):
         plt.plot(SUM_p_Q_array[m])
         plt.hold(True)
     plt.hold(False)
-    plt.savefig('./plots/Sum_p_Q_Iter_ASL.png')
+    plt.savefig('./plots/Sum_p_Q_Iter_ASL.png')"""
     plt.figure(M + 7)
     plt.plot(FE, label='Free energy')
     plt.legend(loc=4)