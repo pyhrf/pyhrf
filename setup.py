c342f65d96a412d1dac6ada799737128d3e4c2b9
Thomas Perret <thomas.perret@inria.fr>
2016-07-07 16:08:45 +0200

    Bump to 0.4.2 version

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ cExtensions = [
 
 setup(
     name = "pyhrf",
-    version = "0.4.1.post1",
+    version = "0.4.2",
     description = ("PyHRF is a set of tools to analyze fMRI data and "
                    "specifically study hemodynamics."),
     long_description = open("README.rst").read(),
