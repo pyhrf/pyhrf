146d41c1a34ae13034ae4460413001d07012b574
Jaime Arias <jaime.arias@inria.fr>
2016-10-25 15:01:32 +0200

    adding AUTHORS.rst file and adding documentation for the setup.py script

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python2
-
+"""Setup script for PyHRF"""
+from __future__ import print_function
 import sys
 
 # setuptools is used to build and distribute pyhrf
-from  ez_setup import use_setuptools
+from ez_setup import use_setuptools
+
 use_setuptools()
 
 # The current version of pyhrf works with python 2.6 or 2.7
@@ -12,17 +14,20 @@ if sys.version_info[:2] < (2, 6) or (3, 0) <= sys.version_info[:2]:
 
 
 def parse_setuppy_commands():
-    """Check the commands and respond appropriately.  Disable broken
-    commands.  Return a boolean value for whether or not to run the
-    build or not (avoid installing numpy)
+    """Check the commands and respond appropriately. Disable broken
+    commands.
+
+    Returns:
+        bool: Return a boolean value for whether or not to run the
+        build (avoid installing numpy and other dependencies)
     """
     if len(sys.argv) < 2:
         # User forgot to give an argument probably, let setuptools handle that.
         return True
 
-    good_commands = ('develop', 'install', 'sdist', 'build', 'build_ext', 'build_py',
-                     'build_clib', 'build_scripts', 'bdist_wheel', 'bdist_rpm',
-                     'bdist_wininst', 'bdist_msi', 'bdist_mpkg',
+    good_commands = ('develop', 'install', 'sdist', 'build', 'build_ext',
+                     'build_py', 'build_clib', 'build_scripts', 'bdist_wheel',
+                     'bdist_rpm', 'bdist_wininst', 'bdist_msi', 'bdist_mpkg',
                      'build_sphinx')
 
     for command in good_commands:
@@ -33,6 +38,8 @@ def parse_setuppy_commands():
 
 
 def setup_package():
+    """Configuration of the setup"""
+
     if parse_setuppy_commands():
         from setuptools import setup
         extra_setuptools_args = dict()
@@ -54,53 +61,52 @@ def setup_package():
 
         import numpy
 
-        cExtensions = [Extension(ext_name,
-                                 sources=['src/pyhrf/'+filepath])
-                       for (ext_name,filepath) in [('pyhrf.jde.intensivecalc','jde/intensivecalc.c'),
-                                                   ('pyhrf.boldsynth.pottsfield.pottsfield_c', 'boldsynth/pottsfield/pottsField.c'),
-                                                   ('pyhrf.vbjde.UtilsC', 'vbjde/utilsmodule.c'),
-                                                   ('pyhrf.cparcellation','cparcellation.c')]]
+        c_extensions = [Extension(ext_name, sources=['src/pyhrf/' + filepath])
+                        for (ext_name, filepath) in
+                        [('pyhrf.jde.intensivecalc', 'jde/intensivecalc.c'),
+                         ('pyhrf.boldsynth.pottsfield.pottsfield_c',
+                          'boldsynth/pottsfield/pottsField.c'),
+                         ('pyhrf.vbjde.UtilsC', 'vbjde/utilsmodule.c'),
+                         ('pyhrf.cparcellation', 'cparcellation.c')]]
 
         # Dependencies of the package
         dependencies.extend(['matplotlib>=1.1',
                              'colorama'])
-        
+
         extra_setuptools_args = dict(
-            package_dir = {'' : 'python'},
-            packages = find_packages("python"),
-            include_package_data = True,
-            scripts = glob('./bin/*'),
-            zip_safe = False,  # pyhrf has C/C++ extensions, so it's not zip safe.
-            ext_modules = cExtensions,
-            include_dirs = [numpy.get_include()],
-            setup_requires = dependencies,
-            install_requires = dependencies,
-            extras_require = {"Ward": ["scikit-learn>=0.10"],
-                              "parallel": ["joblib>=0.5"],
-                              "cluster": ["soma-workflow"],
-                              "simulation": ["Pillow>=2.3"],
-                              "parcellation": ["munkres>=1.0"],
-                              "pipelines": ["pygraphviz>=1.1"],
-                              "graph": ["python-graph-core>=1.8"]}
+            package_dir={'': 'python'},
+            packages=find_packages("python"),
+            include_package_data=True,
+            scripts=glob('./bin/*'),
+            zip_safe=False,  # pyhrf has C/C++ extensions, so it's not zip safe.
+            ext_modules=c_extensions,
+            include_dirs=[numpy.get_include()],
+            setup_requires=dependencies,
+            install_requires=dependencies,
+            extras_require={"Ward": ["scikit-learn>=0.10"],
+                            "parallel": ["joblib>=0.5"],
+                            "cluster": ["soma-workflow"],
+                            "simulation": ["Pillow>=2.3"],
+                            "parcellation": ["munkres>=1.0"],
+                            "pipelines": ["pygraphviz>=1.1"],
+                            "graph": ["python-graph-core>=1.8"]}
         )
 
     # Get the long description from the README file
-    with open('README.rst') as f:
-        long_description = f.read()
+    with open('README.rst') as readme_file:
+        long_description = readme_file.read()
 
-    metadata = dict (
-        name = 'pyhrf',
-        version = '0.4.3',
-        description = 'Set of tools to analyze fMRI data focused on the study of hemodynamics',
-        long_description = long_description,
-        author = 'Thomas Vincent, Philippe Ciuciu, Solveig Badillo, Florence Forbes, Aina Frau-Pascual, Thomas Perret',
-        author_email = "thomas.tv.vincent@gmail.com",
-        maintainer = 'Jaime Arias',
-        maintainer_email = 'jaime.arias@inria.fr',
-        url = 'http://pyhrf.org',
-        license = 'CeCILLv2',
-        download_url = 'https://github.com/pyhrf/pyhrf',
-        classifiers = [
+    metadata = dict(
+        name='pyhrf',
+        version='0.4.3',
+        description='Analysis of fMRI data based on the study of hemodynamics',
+        long_description=long_description,
+        maintainer='Jaime Arias',
+        maintainer_email='jaime.arias@inria.fr',
+        url='http://pyhrf.org',
+        license='CeCILLv2',
+        download_url='https://github.com/pyhrf/pyhrf',
+        classifiers=[
             "Development Status :: 3 - Alpha",
             "Environment :: Console",
             "Intended Audience :: Science/Research",
@@ -116,7 +122,7 @@ def setup_package():
             "Topic :: Scientific/Engineering :: Mathematics",
             "Topic :: Scientific/Engineering :: Medical Science Apps.",
         ],
-        platforms = ["Linux"],
+        platforms=["Linux"],
         **extra_setuptools_args
     )
 
@@ -128,10 +134,23 @@ if __name__ == '__main__':
 
     if 'install' in sys.argv[1]:
         import colorama
+
         colorama.init(autoreset=True)
-        def red(text): return "{}{}".format(colorama.Fore.RED, text)
-        def green(text): return "{}{}".format(colorama.Fore.GREEN, text)
-        def yellow(text): return "{}{}".format(colorama.Fore.YELLOW, text)
+
+        def red(text):
+            """Return a red terminal text"""
+            return "{}{}".format(colorama.Fore.RED, text)
+
+
+        def green(text):
+            """Return a green terminal text"""
+            return "{}{}".format(colorama.Fore.GREEN, text)
+
+
+        def yellow(text):
+            """Return a yellow terminal text"""
+            return "{}{}".format(colorama.Fore.YELLOW, text)
+
 
         def check_opt_dep(dep_name, dep_descrip):
             """
@@ -141,25 +160,29 @@ if __name__ == '__main__':
             try:
                 __import__(dep_name)
             except ImportError:
-                return red("{} is *NOT IMPORTABLE*, {} will *NOT* be available".format(dep_name,
-                                                                                       dep_descrip))
-            
-            return green("{} is importable, {} will be available".format(dep_name, dep_descrip))
-        
+                return red(
+                    "{} is *NOT IMPORTABLE*, {} will *NOT* be available".format(
+                        dep_name,
+                        dep_descrip))
+
+            return green(
+                "{} is importable, {} will be available".format(dep_name,
+                                                                dep_descrip))
+
+
         # Optional deps and description of associated feature:
-        optional_deps = {
-            "sklearn": "(scikit-learn) -- spatial ward parcellation",
-            "joblib": "local parallel feature (eg pyhrf_jde_estim -x local)",
-            "soma_workflow": "cluster parallel feature (eg pyhrf_jde_estim -x cluster)",
-            "PIL": "loading of image file as simulation maps",
-            "munkres": "computation of distance between parcellations",
-            "pygraph": "(python-graph-core) -- save plot of simulation pipelines",
-            "pygraphviz": "optimized graph operations and outputs",
+        OPTIONAL_DEPS = {
+            'sklearn': '(scikit-learn) -- spatial ward parcellation',
+            'joblib': 'local parallel feature (eg pyhrf_jde_estim -x local)',
+            "soma_workflow": 'cluster parallel feature (eg pyhrf_jde_estim -x cluster)',
+            'PIL': "loading of image file as simulation maps",
+            'munkres': 'computation of distance between parcellations',
+            'pygraph': '(python-graph-core) -- save plot of simulation pipelines',
+            'pygraphviz': 'optimized graph operations and outputs',
         }
 
-        print yellow("\nOptional dependencies:")
-        print "\n".join(["- "+ check_opt_dep(dn, dd) for dn, dd in optional_deps.items()])
-        
-        print yellow("\nIf the installation was successfull, you may run pyhrf_maketests to run package tests.\n")
-                
+        print(yellow('\nOptional dependencies:'))
+        print('\n'.join(
+            ['- ' + check_opt_dep(dn, dd) for dn, dd in OPTIONAL_DEPS.items()]))
 
+        print(yellow('\nExecute pyhrf_maketests to run package tests.\n'))
